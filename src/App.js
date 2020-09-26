82d0d69ce8027272f25f99e73956387e1e7019db
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-09-26 12:57:03 -0300

    feat: create deleteNote method

--- src/App.js
+++ src/App.js
@@ -22,11 +22,23 @@ class App extends Component {
     this.setState(novoEstado);
   }
 
+  deletarNota(index) {
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index, 1);
+    this.setState({ notas: arrayNotas });
+  }
+
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro newNote={this.newNote.bind(this)} />
-        <ListaDeNotas notas={this.state.notas} />
+        <main>
+          <ListaDeCategorias />
+          <ListaDeNotas
+            apagarNota={this.deletarNota.bind(this)}
+            notas={this.state.notas}
+          />
+        </main>
       </section>
     );
   }
---
+++ src/components/ListaDeCategorias/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ListaDeCategorias extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default ListaDeCategorias;
